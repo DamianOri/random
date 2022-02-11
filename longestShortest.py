9907ecc9590b6830ee8680a729422813edaab671
DamianOri <99432341+DamianOri@users.noreply.github.com>
2022-02-11 12:32:54 +0100

    Update and rename longestWord.py to longestShortest.py
    Now it can check for both the longest and shortest word. However if there are 2 words with the same length then it just takes the first one

---
+++ longestShortest.py
@@ -0,0 +1,19 @@
+# 10 II 2022, 0.1
+
+input_string = input('Enter the words you want to check seperated by spaces (NOT COMMAS): \n') 
+words = input_string.split() 
+
+long = words[0] 
+short = words[0]
+for i in range(len(words)): 
+    x = words[i] 
+
+    if(len(x) > len(long)):
+        long = x
+    if(len(x) < len(short)):
+        short = x
+
+print(f"Longest: {long} \n")
+print(f"Shortest: {short} \n")
+
+input('Press anything to exit.')
