c6195dd5e23bf481fe9124092867e36a54a463e6
DamianOri <99432341+DamianOri@users.noreply.github.com>
2022-02-11 17:50:59 +0100

    Create veryshortones.py
    things i thought would took me 2 hours to write but actually are only like 2 lines long

---
+++ veryshortones.py
@@ -0,0 +1,13 @@
+# Fast Inverse Square Root
+x = input()
+print(x**-0.5)
+
+# Spelling out words
+word = input()
+print(list(word))
+
+# Cheap uwu talk
+x = input()
+x = x.replace('r', 'w')
+x = x.replace('l', 'w')
+print(x.replace('u', 'w'))
