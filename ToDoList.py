e0128fceb66e2432f6b3a48fc837ae91530b7f4f
DamianOri <99432341+DamianOri@users.noreply.github.com>
2022-03-06 11:43:42 +0100

    To-Do list 2.0
    Makes the commands better and case insensitive, adds a list read option and allows using a custom file.

--- ToDoList.py
+++ ToDoList.py
@@ -1,17 +1,26 @@
-print("Type EXIT to exit.")
-print("Type ERASE to erase the list.")
-todo = "file path to a .txt file"        #sets todo as the file in which the list is stored
-x = True                                 #for the while loop
-a = open(todo, "a")                      #opens the todo file and sets it in append mode (add text to the end of the file)
+# Version 2.0
+# 6 III 2022
+print("To-Do list program, by Damian.\n")
+todo = input("Input the path to the .txt file in which the todo list is saved\n")
+print("Type /exit to exit.")
+print("Type /erase to erase the list.")
+print("Type /read to display the list")
+x = True                                        # For the while loop
+a = open(todo, "a")                             # Opens the file in append mode (adds stuff at the end of the file)
 while(x == True):
     stuff = input()
-    if(stuff == "EXIT"):
-        a.close()                        #closes the file and ends the while loop, closing the program
-        x = False
-    elif(stuff == "ERASE"):
-        a = open(todo, "w")              #sets the todo file in write mode (overwrites the whole file)
-        a.write("")                      #erases everything
-        a = open(todo, "a")              #goes back to append mode
+    if(stuff.lower() == "/exit"):               # .lower() added so it's case insensitive
+        a.close()                               # Closes the file
+        x = False                               # End the program
+        input("Press Enter to leave")
+    elif(stuff.lower() == "/erase"):
+        a = open(todo, "w")                     # Changes to write mode (overwriting the whole file)            
+        a.write("")                             # Erases everything
+        a = open(todo, "a")                     # Goes back to append mode
         print("List erased.")
+    elif(stuff.lower() == "/read"):
+        a = open(todo, "r")                     # Changes to read mode
+        print(f"To-Do list:\n{a.read()}")       # Prints the list
+        a = open(todo, "a")                     # Back to append mode
     else:
-        a.write(stuff+"\n")              #writes whatever the user wants and adds a new line so that the items are in seperate lines
+        a.write(stuff+"\n")                     # Adds whatever the user writes at the end of the list and changes to the next line so everything is written in seperate lines
