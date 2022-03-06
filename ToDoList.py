35190726ae89e853e70266fb8f1f77c5c7ea5e7e
DamianOri <99432341+DamianOri@users.noreply.github.com>
2022-03-06 12:07:01 +0100

    To-Do list 2.0.1
    Fixes the bug where /erase only works on the second time it's used

--- ToDoList.py
+++ ToDoList.py
@@ -1,4 +1,4 @@
-# Version 2.0
+# Version 2.0.1
 # 6 III 2022
 print("To-Do list program, by Damian.\n")
 todo = input("Input the path to the .txt file in which the todo list is saved\n")
@@ -17,6 +17,9 @@ while(x == True):
         a = open(todo, "w")                     # Changes to write mode (overwriting the whole file)            
         a.write("")                             # Erases everything
         a = open(todo, "a")                     # Goes back to append mode
+        a = open(todo, "w")
+        a.write("")
+        a = open(todo, "a")                     # Does it for the second time because it doesn't work the first time for some reason
         print("List erased.")
     elif(stuff.lower() == "/read"):
         a = open(todo, "r")                     # Changes to read mode
