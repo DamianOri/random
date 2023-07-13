c50095fc5a913da37196d301817b2aa0e9e5dfe3
DamianOri <99432341+DamianOri@users.noreply.github.com>
2023-07-13 10:38:21 +0200

    Update guessingGame.py
    Changed variable names to something more descriptive.

--- guessingGame.py
+++ guessingGame.py
@@ -1,23 +1,23 @@
 # Guessing game v1.1
 # Damian, 13-07-2023
 import random
-a = 0
-x = random.randint(1, 100)
+count = 0
+randNum = random.randint(1, 100)
 
-print("In this \"game\" you need to guess a randomly generated integer from 1 to 100.\nThankfully your job is made a bit easier by the computer telling you if your guess is too high or too low")
+print("In this \"game\" you need to guess a randomly generated integer from 1 to 100.\nThankfully your job is made a bit easier by the computer telling you if your guess is too high or too low.\nYou can type \"exit\" to leave.")
 
 while True:
-    a += 1
-    y = input("Input a guess: ")
-    if(y.lower() == "exit"):
+    count += 1
+    guess = input("Input a guess: ")
+    if(guess.lower() == "exit"):
         quit()
-    elif(y.isdigit() == False):
+    elif(guess.isdigit() == False):
         print("Input an integer, smh.")
-    elif(int(y) < x):
+    elif(int(guess) < randNum):
         print("Too low")
-    elif(int(y) > x):
+    elif(int(guess) > randNum):
         print("Too high")
     else:
         break
-print(f"Congrats! You guessed the number! You needed {a} guesses.")
+print(f"Congrats! You guessed the number! You needed {count} guesses.")
 input("Press ENTER to leave.")
